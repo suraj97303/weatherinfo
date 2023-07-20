8401f3978e02c893bd9c5ecc4bec9e46c612a84e
Suraj Mishra <91696589+suraj97303@users.noreply.github.com>
2023-07-20 21:18:45 +0530

    done

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "7c64ad7a78ebb2dee2ae421b74068aba",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
